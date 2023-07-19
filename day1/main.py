16b93f1d8b7adfbf10b40d3b2cce55df87b26a19
lottt105 <hyobin0601@naver.com>
2023-07-19 13:46:44 +0900

    python 실습 (문자열)

---
+++ day1/main.py
@@ -0,0 +1,56 @@
+# print("hello")
+# a = 10
+# b = 5
+
+# print(a + b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a ** b)
+# print("*" * 100)
+
+# happy_face = '☺'
+# print(happy_face)
+
+# s = "256"
+# print(int(s), type(int(s)))
+# print(float(s), type(float(s)))
+
+# random_list  = [3, 3.9, "python", False, True]
+# print(random_list)
+
+# for element in random_list:
+#   print(type(element))
+  
+# aa = "Hello world"
+# bb = "ESG"
+# my_list = [1, 2, 3, 4]
+# print(len(aa))
+# print(len(bb))
+# print(len(my_list))
+
+# print(aa[0:1])
+# print(aa[0:2])
+# print(aa[0:3])
+# print(aa[0:6])
+
+# find_str = "python!"
+# print(find_str.find("y"))
+
+# title = "       PytHon ClasS      "
+# print(title.lower())
+# print(title.upper())
+# print(title.strip().lower())
+
+# find_love = "I love lyou"
+# print(find_love.find("love"))
+
+quote = "Life if too short"
+print(quote.replace("too", "very"))
+print(quote.replace("o", "0"))
+print(quote.split())
+
+split_str = "a:b:c:d"
+print(split_str.split(":"))
+split_a = "Iaknowasplit"
+print(split_a.split("a"))
---
+++ day1/practice.py
@@ -0,0 +1,16 @@
+# 1. 특정 문자열 출력
+str_dog = "Dog is love"
+idx = str_dog.find("love")
+print(str_dog[idx:idx + 4])
+
+# 2. 특정 문자열 바꿔서 출력
+print(str_dog.replace("Dog", "Cat"))
+
+# 3. 문자열 길이 구하고, 특정 문자 개수 출력
+str_la = "lalalalalalalalalalala"
+print(len(str_la))
+print(str_la.count("a"))
+
+# 4. 특정 문자 제거하고 출력
+str_tel = "010-1234-5678"
+print(str_tel.replace("-", ""))
